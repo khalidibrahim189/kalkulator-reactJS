71639e784be7b1f4dd26eb260e7c13eb2a70b0bf
Khalid Ibrahim <khalid.jambi18@gmail.com>
2021-07-07 20:44:10 +0700

    kalkulator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Kalkulator from './Kalkulator'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Kalkulator />
+    </div>
+  )
+}
+
+export default App
